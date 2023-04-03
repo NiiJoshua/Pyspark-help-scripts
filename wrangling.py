c50c638c1f34b77158db6db9411b55e8226d004d
NiJoshua <joshkobeng@gmail.com>
2023-04-03 14:21:18 +0000

    update

--- wrangling.py
+++ wrangling.py
@@ -7,3 +7,8 @@ concat_udf = udf(lambda x: [i for i in x if i is not None], ArrayType(StringType
 concat_col = concat_ws(',',"fastest_player_filters","top_predictor_filters","top_quizzer_filters","top_player_filters")
 array_col = concat_udf(array(concat_col))
 leaderboard_avatar_df = users_df_with_filters.withColumn("avatar_label", array_col).withColumn("attributes",split(col("avatar_label")[0],','))
+
+# Filter an array column with attributes
+filtered_table = (new_silver_users
+                    .filter(array_contains(col("attributes"), "top-quizzer") | array_contains(col("attributes"), "top-predictor") | array_contains(col("attributes"), "fastest-player") | array_contains(col("attributes"), "top-player"))
+)
